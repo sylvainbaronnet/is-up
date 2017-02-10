0d89bffdf9bf831d74e4c7ebc6a11b42506f811f
Sylvain Baronnet <sylvain@studio.gd>
2017-02-10 21:21:30 +0100

    .

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ var got = require('got');
 
 module.exports = function (url) {
 	return got('http://isitup.org/' + url + '.json', {
-		json: true,
-		headers: {
-			'user-agent': 'https://github.com/sindresorhus/is-up'
-		}
+		json: true
 	}).then(function (res) {
 		if (res.body.status_code === 3) {
 			throw new Error('Invalid domain');
